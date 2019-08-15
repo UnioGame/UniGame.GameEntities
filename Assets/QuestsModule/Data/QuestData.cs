54f51055daf91c3a3dc382c1d1c5f845b304ac60
Glongooo <glongooo4@gmail.com>
2019-08-15 18:20:07 +0300

    квестовый скелет

---
+++ Assets/QuestsModule/Data/QuestData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GBG.Modules.Quests.Data
+{
+    public enum QuestState
+    {
+        InProgress,
+        RewardAvailable,
+        Done
+    }
+
+    public class QuestData
+    {
+
+    }
+}
---
+++ Assets/QuestsModule/QuestsController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class QuestsController : MonoBehaviour
+{
+    private void RerandomQuests()
+    {
+        throw new NotImplementedException();
+    }
+
+
+}
